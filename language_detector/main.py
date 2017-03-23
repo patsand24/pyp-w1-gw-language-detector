353685990c7c4d32a983c310b30805b608de8632
Pat <patsand24csu@yahoo.com>
2017-03-23 23:25:36 +0000

    Partial, still not valid solution

--- language_detector/main.py
+++ language_detector/main.py
@@ -1,8 +1,8 @@
-# -*- coding: utf-8 -*-
+mkvirt# -*- coding: utf-8 -*-
 
 """This is the entry point of the program."""
 
 def detect_language(text, languages):
     """Returns the detected language of given text."""
     # implement your solution here
-    pass
+    return 'Spanish'
